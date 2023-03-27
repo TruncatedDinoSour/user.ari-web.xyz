c72e60b101b0b85abab9868b5cf6ea7f1f5509e1
Ari Archer <ari.web.xyz@gmail.com>
2023-03-27 17:43:14 +0300

    improve the client and js
    Signed-off-by: Ari Archer <ari.web.xyz@gmail.com>

--- content/index.js
+++ content/index.js
@@ -72,92 +72,81 @@ async function load_comments(comments, noscroll) {
     let old_comments = comments.comments;
     comments.comments = Math.max(0, old_comments - COMMENTS_DEC + 1);
 
-    api(`${comments.comments}/${old_comments}`)
-        .then((r) => r.json())
-        .then((j) => {
-            let k;
+    let j = await (await api(`${comments.comments}/${old_comments}`)).json();
+    let k;
 
-            for (k of Object.keys(j).reverse()) {
-                let c = j[k];
-                comments.appendChild(new_comment(k, c[0], c[1]));
-            }
+    for (k of Object.keys(j).reverse())
+        comments.appendChild(new_comment(k, ...j[k]));
 
-            if (!noscroll) window.scrollTo(0, document.body.scrollHeight);
-            if (!k || k === "1") return;
+    if (!noscroll) window.scrollTo(0, document.body.scrollHeight);
+    if (!k || k === "1") return;
 
-            let li = document.createElement("li");
-            let button = document.createElement("button");
+    let li = document.createElement("li");
+    let button = document.createElement("button");
 
-            button.innerText = "press to load more ...";
+    button.innerText = "press to load more ...";
 
-            li.setAttribute("data-readmore", true);
-            li.appendChild(button);
+    li.setAttribute("data-readmore", true);
+    li.appendChild(button);
 
-            li.onclick = async () => {
-                li.remove();
-                await load_comments(comments);
-            };
+    li.onclick = async () => {
+        li.remove();
+        await load_comments(comments);
+    };
 
-            comments.appendChild(li);
-        })
-        .catch((e) => {
-            throw e;
-        });
+    comments.appendChild(li);
+
+    if (!noscroll) window.scrollTo(0, document.body.scrollHeight);
+
     comments.comments--;
 }
 
-async function load_comment_field(comments, old_comments_count) {
+async function load_comment_field(comments) {
     let comment = document.getElementById("comment");
     let send = document.getElementById("send-comment");
 
     send.onclick = async () => {
-        if (!comment.value) return;
+        if (!(comment.value = comment.value.trim())) return;
 
         let data = new FormData();
 
         data.set("author", window.localStorage["username"]);
         data.set("content", comment.value);
 
-        api("/", {
-            method: "POST",
-            body: data,
-        })
-            .then((r) => r.text())
-            .then((d) => {
-                comment.value = "";
+        let comment_id = await (
+            await api("/", { method: "POST", body: data })
+        ).text();
 
-                document.getElementById("count").innerText = d;
-                comments.comments = parseInt(d);
+        comment.value = "";
 
-                if (
-                    !comments.skipped &&
-                    comments.comments - 1 > old_comments_count
-                ) {
-                    comments.skipped = true;
+        document.getElementById("count").innerText = comment_id;
+        comments.comments = parseInt(comment_id);
 
-                    let li = document.createElement("li");
-                    let i = document.createElement("i");
+        if (comments.comments - 1 > comments.old_comments) {
+            let li = document.createElement("li");
+            let i = document.createElement("i");
 
-                    i.innerText = `skipped ${
-                        comments.comments - old_comments_count - 1
-                    } comment( s )`;
+            i.innerText = `skipped ${
+                comments.comments - comments.old_comments - 1
+            } comment(s)`;
 
-                    li.setAttribute("data-skip", true);
-                    li.appendChild(i);
+            li.setAttribute("data-skip", true);
+            li.appendChild(i);
 
-                    comments.prepend(li);
-                }
-                comments.prepend(
-                    new_comment(d, data.get("author"), data.get("content"))
-                );
-            })
-            .catch(() => window.location.reload());
+            comments.prepend(li);
+        }
+
+        comments.old_comments = comments.comments;
+
+        comments.prepend(
+            new_comment(comment_id, data.get("author"), data.get("content"))
+        );
     };
 }
 
 async function handle_auth() {
-    let username = window.localStorage.getItem("username");
-    while (!username) username = prompt("username");
+    let username = window.localStorage.getItem("username")?.trim();
+    while (!username) username = prompt("username")?.trim();
     window.localStorage.setItem("username", username);
 
     document
@@ -169,15 +158,16 @@ async function main() {
     await handle_auth();
 
     let comments = document.getElementById("comments");
-    let comment_count = await (await api("total")).text();
+
+    comments.old_comments = await (await api("total")).text();
 
     comments.comments = window.location.hash
         ? window.location.hash.slice(1)
-        : comment_count;
-    document.getElementById("count").innerText = comment_count;
+        : comments.old_comments;
+    document.getElementById("count").innerText = comments.old_comments;
 
-    await load_comment_field(comments, comments.comments);
-    await load_comments(comments, comment_count, true);
+    await load_comment_field(comments);
+    await load_comments(comments, true);
 }
 
 document.addEventListener("DOMContentLoaded", main);
